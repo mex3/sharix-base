f136da8becce67cabcbcef48d6cad16ce50f82ed
Sergo18 <51698569+Sergo18@users.noreply.github.com>
2019-07-15 16:19:12 +0300

    new DbOperations script

--- android-app/register/Android/includes/DbOperations.php
+++ android-app/register/Android/includes/DbOperations.php
@@ -18,6 +18,9 @@
 		/*CRUD -> C -> CREATE */
 
 		public function createUser($id, $username, $pass, $email){
+			if($this->isUserExist($username,$email)){
+                return 0; 
+            }else{
 				$password = md5($pass);
 				$id = time();
 				$stmt = $this->con->prepare("INSERT INTO `signup` (`id`, `Username`, `Password`, `Email`) VALUES (?, ?, ?, ?);");
@@ -29,5 +32,13 @@
 					return 2;
 				}
 			}
+		}
+		
+		private function isUserExist($username, $email){
+        $stmt = $this->con->prepare("SELECT id FROM users WHERE username = ? OR email = ?");
+        $stmt->bind_param("ss", $username, $email);
+        $stmt->execute(); 
+        $stmt->store_result(); 
+        return $stmt->num_rows > 0; 
+        }
 	}
-
