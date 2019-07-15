9f66f8379019b17e78d3921a94f100593dbb118d
Sergo18 <51698569+Sergo18@users.noreply.github.com>
2019-07-15 16:38:08 +0300

    new

--- android-app/register/Android/v1/registerUser.php
+++ android-app/register/Android/v1/registerUser.php
@@ -15,7 +15,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 
 		$db = new DbOperations(); 
 
-		$result = $db->createUser( NULL,	$_POST['username'],
+		$result = $db->createUser( NULL,$_POST['username'],
 									$_POST['password'],
 									$_POST['email']
 								);
