c7eabe43fd6d404849aa725b40f70d2f54a7a15d
Sergo18 <51698569+Sergo18@users.noreply.github.com>
2019-07-15 18:45:57 +0300

    new script for lo user

--- android-app/register/Android/includes/DbOperations.php
+++ android-app/register/Android/includes/DbOperations.php
@@ -34,6 +34,22 @@
 			}
 		}
 		
+		public function userLogin($username, $pass){
+			$password = md5($pass);
+            $stmt = $this->con->prepare("SELECT id FROM signup WHERE Username = ? AND Password = ?");
+            $stmt->bind_param("ss",$username,$password);
+            $stmt->execute();
+            $stmt->store_result(); 
+            return $stmt->num_rows > 0; 
+        }
+ 
+        public function getUserByUsername($username){
+            $stmt = $this->con->prepare("SELECT * FROM signup WHERE Username = ?");
+            $stmt->bind_param("s",$username);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc();
+        }
+		
 		private function isUserExist($username, $email){
 			$stmt = $this->con->prepare("SELECT id FROM signup WHERE Username = ? OR Email = ?");
 			$stmt->bind_param("ss", $username, $email);
